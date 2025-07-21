169b5084495c33a9e85ee2883247829578785769
dragonplus8 <125908478+dragonplus8@users.noreply.github.com>
2025-07-21 22:23:20 +0100

    Add files via upload

---
+++ Note 21 Jul 2025 at 21_58_02 2.js
@@ -0,0 +1,17 @@
+{
+    "sourceName": "HAnime",
+    "iconUrl": "https://raw.githubusercontent.com/50n50/sources/refs/heads/main/hianime/iconapp.png",
+    "author": {
+        "name": "bob",
+        "icon": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3122kQwublLkZ6rf1fEpUP79BxZOFmH9BSA&s"
+    },
+    "version": "1.0",
+    "language": "English (SUB)",
+    "streamType": "HLS",
+    "quality": "1080p",
+    "baseUrl": "https://hanime.tv/api/v8/browse-trending?time=${time}&page=1&order_by=views&ordering=desc",
+    "searchBaseUrl": "https://bshar1865-hianime.vercel.app/api/v2/hianime/search?q=%s",
+    "scriptUrl": "",
+    "type": "anime",
+    "asyncJS": true,
+    "softsub": true
